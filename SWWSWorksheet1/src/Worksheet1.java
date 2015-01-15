1a7c5fc989392ee5d7ba309e5ebbe18182f93137
lorilew <lorilew123@gmail.com>
2015-01-15 15:59:30 +0000

    Added method allPositive to worksheet1

--- SWWSWorksheet1/src/Worksheet1.java
+++ SWWSWorksheet1/src/Worksheet1.java
@@ -4,29 +4,53 @@
  */
 
 public class Worksheet1 {
-
+	/**
+	 * Given a list of integers a,
+	 * checks all values in List, if all positive (>=0) 
+	 * returns a boolean true, else returns a boolean false.
+	 * @param a The list to check all positive.
+	 * @return A boolean true if all values are positive, else false.
+	 */
 	public static boolean allPositive(List a) {
+		if(a.isEmpty()){
+			throw new IllegalStateException();
+		}
+		else if(a.getTail().isEmpty()){
+			if(a.getHead()>=0){
+				return true;
+			}
+			return false;
+		}
+		else{
+			if(a.getHead()>=0){
+				return true && allPositive(a.getTail());
+			}
+			return false;
+		}
 	}
-
-	public static boolean sorted(List a) {
-	}
-
+	/**
+	 * Given a list of integers a
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static List merge(List a, List b) {
+		
 	}
-
-	static List removeDuplicates(List a) {
-	}
-
-	public static Tree mirror(Tree t) {
-	}
-
-	public static String showDescending(Tree a) {
-	}
-
-	public static int max(Tree a) {
-	}
-
-	public static Tree delete(Tree a, int x) {
-	}
+//
+//	static List removeDuplicates(List a) {
+//	}
+//
+//	public static Tree mirror(Tree t) {
+//	}
+//
+//	public static String showDescending(Tree a) {
+//	}
+//
+//	public static int max(Tree a) {
+//	}
+//
+//	public static Tree delete(Tree a, int x) {
+//	}
 
 }
--- SWWSWorksheet1/src/Worksheet1Test.java
+++ SWWSWorksheet1/src/Worksheet1Test.java
@@ -7,5 +7,63 @@ import org.junit.Test;
  * This class contains the test cases for Worksheet1 solutions.
  */
 public class Worksheet1Test {
+	
+	@Test
+	/*
+	 * Testing allPositive(List a) with empty List.
+	 */
+	public void ex1test1(){
+		List a = List.empty();
+		try{
+			Worksheet1.allPositive(a);
+			assert(false);
+		} catch (IllegalStateException e){
+			assert(true);
+		}
+	}
+	@Test
+	/*
+	 * Testing allPositive(List a) with value 0.
+	 */
+	public void ex1test2(){
+		List a = List.cons(0, List.empty());
+		boolean result = Worksheet1.allPositive(a);
+		assertTrue(result);
+	}
+	@Test
+	/*
+	 * Testing allPositive(List a) with value 1 , 2, 3.
+	 */
+	public void ex1test3(){
+		List a = List.cons(1,List.cons(3, List.cons(2,List.empty())));
+		boolean result = Worksheet1.allPositive(a);
+		assertTrue(result);
+	}
+	/*
+	 * Testing allPositive(List a) with value 1 , -2, 3.
+	 */
+	public void ex1test4(){
+		List a = List.cons(1,List.cons(3, List.cons(-2,List.empty())));
+		boolean result = Worksheet1.allPositive(a);
+		assertFalse(result);
+	}
+	/*
+	 * Testing allPositive(List a) with value -1 , -2, 3.
+	 */
+	public void ex1test5(){
+		List a = List.cons(-1,List.cons(3, List.cons(-2,List.empty())));
+		boolean result = Worksheet1.allPositive(a);
+		assertFalse(result);
+	}
+	/*
+	 * Testing allPositive(List a) with value -1 , 0, -3.
+	 */
+	public void ex1test6(){
+		List a = List.cons(-1,List.cons(3, List.cons(0,List.empty())));
+		boolean result = Worksheet1.allPositive(a);
+		assertFalse(result);
+	}
+	/////////////////////////////////////////////////////////////////////////
+	
 
 }
