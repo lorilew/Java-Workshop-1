c10104978aecf63ae9b72640a40c62da83a5c483
lorilew <lorilew123@gmail.com>
2015-01-15 17:40:21 +0000

    Added sorted() and Merge()

--- SWWSWorksheet1/src/Worksheet1.java
+++ SWWSWorksheet1/src/Worksheet1.java
@@ -8,7 +8,7 @@ public class Worksheet1 {
 	 * Given a list of integers a,
 	 * checks all values in List, if all positive (>=0) 
 	 * returns a boolean true, else returns a boolean false.
-	 * @param a The list to check all positive.
+	 * @param a The list of integers.
 	 * @return A boolean true if all values are positive, else false.
 	 */
 	public static boolean allPositive(List a) {
@@ -29,14 +29,62 @@ public class Worksheet1 {
 		}
 	}
 	/**
-	 * Given a list of integers a
-	 * @param a
-	 * @param b
-	 * @return
+	 * Given a List of integers a, this method returns a boolean value
+	 * indicating whether a is sorted in ascending order.
+	 * It may contain duplicate values.
+	 * @param a A list of integers.
+	 * @return A boolean indicating whether list is sorted.
+	 */
+	public static boolean sorted(List a){
+		if(a.isEmpty()){
+			throw new IllegalStateException();
+		}
+		else if(a.getTail().isEmpty()){
+			return true;
+		}
+		else{
+			if(a.getHead()<=a.getTail().getHead()){
+				return true && sorted(a.getTail());
+			}
+			return false;
+		}
+	}
+	/**
+	 * REQUIRED: a and b must be sorted lists of integers.
+	 * Given two sorted lists a and b, the method returns a new sorted list
+	 * that contains all the elements of a and b, any duplicate copies of
+	 * elements in a or b or their combination are retained.
+	 * Example:
+	 * a: [2,5,5,8] b: [5,7,8,9]  ==> [2,5,5,5,7,8,8,9]
+	 * @param a A sorted list of integers.
+	 * @param b A sorted list of integers.
+	 * @return A new sorted list containing all elements from a and b.
 	 */
 	public static List merge(List a, List b) {
+		if(!a.isEmpty() && !b.isEmpty() && !sorted(a) && !sorted(b)){
+			throw new IllegalStateException();
+		} else {
+			return auxMerge(a,b);
+		}
+		
 		
 	}
+	private static List auxMerge(List a, List b){
+		if(a.isEmpty()){
+			return b;
+		}
+		else if(b.isEmpty()){
+			return a;
+		}
+		else if(a.getHead()<b.getHead()){
+			return List.cons(a.getHead(), auxMerge(a.getTail(),b));
+		}
+		else{
+			return List.cons(b.getHead(), auxMerge(a, b.getTail()));
+		}
+	}
+	
+	
 //
 //	static List removeDuplicates(List a) {
 //	}
@@ -52,5 +100,13 @@ public class Worksheet1 {
 //
 //	public static Tree delete(Tree a, int x) {
 //	}
+	private static List getLastElement(List l){
+		if(l.getTail().isEmpty()){
+			return l;
+		}
+		else{
+			return getLastElement(l.getTail());
+		}
+	}
 
 }
--- SWWSWorksheet1/src/Worksheet1Test.java
+++ SWWSWorksheet1/src/Worksheet1Test.java
@@ -8,10 +8,11 @@ import org.junit.Test;
  */
 public class Worksheet1Test {
 	
-	@Test
+	
 	/*
 	 * Testing allPositive(List a) with empty List.
 	 */
+	@Test
 	public void ex1test1(){
 		List a = List.empty();
 		try{
@@ -21,19 +22,21 @@ public class Worksheet1Test {
 			assert(true);
 		}
 	}
-	@Test
+	
 	/*
 	 * Testing allPositive(List a) with value 0.
 	 */
+	@Test
 	public void ex1test2(){
 		List a = List.cons(0, List.empty());
 		boolean result = Worksheet1.allPositive(a);
 		assertTrue(result);
 	}
-	@Test
+	
 	/*
 	 * Testing allPositive(List a) with value 1 , 2, 3.
 	 */
+	@Test
 	public void ex1test3(){
 		List a = List.cons(1,List.cons(3, List.cons(2,List.empty())));
 		boolean result = Worksheet1.allPositive(a);
@@ -42,6 +45,7 @@ public class Worksheet1Test {
 	/*
 	 * Testing allPositive(List a) with value 1 , -2, 3.
 	 */
+	@Test
 	public void ex1test4(){
 		List a = List.cons(1,List.cons(3, List.cons(-2,List.empty())));
 		boolean result = Worksheet1.allPositive(a);
@@ -50,6 +54,7 @@ public class Worksheet1Test {
 	/*
 	 * Testing allPositive(List a) with value -1 , -2, 3.
 	 */
+	@Test
 	public void ex1test5(){
 		List a = List.cons(-1,List.cons(3, List.cons(-2,List.empty())));
 		boolean result = Worksheet1.allPositive(a);
@@ -58,12 +63,132 @@ public class Worksheet1Test {
 	/*
 	 * Testing allPositive(List a) with value -1 , 0, -3.
 	 */
+	@Test
 	public void ex1test6(){
 		List a = List.cons(-1,List.cons(3, List.cons(0,List.empty())));
 		boolean result = Worksheet1.allPositive(a);
 		assertFalse(result);
 	}
 	/////////////////////////////////////////////////////////////////////////
-	
-
+	/*
+	 * Testing sorted(List a) with empty list
+	 */
+	@Test
+	public void ex2test1(){
+		List a = List.empty();
+		try{
+			Worksheet1.sorted(a);
+			assert(false);
+		} catch (IllegalStateException e){
+			assert(true);
+		}
+	}
+	/*
+	 * Testing sorted(List a) with single value list.
+	 */
+	@Test
+	public void ex2test2(){
+		List a = List.cons(1, List.empty());
+		boolean result = Worksheet1.sorted(a);
+		assertTrue(result);
+	}
+	/*
+	 * Testing sorted(List a) with sorted value list.
+	 */
+	@Test
+	public void ex2test3(){
+		List a = List.cons(1, List.cons(3, List.empty()));
+		boolean result = Worksheet1.sorted(a);
+		assertTrue(result);
+	}
+	/*
+	 * Testing sorted(List a) with unsorted value list.
+	 */
+	@Test
+	public void ex2test4(){
+		List a = List.cons(3, List.cons(1, List.cons(5,List.empty())));
+		boolean result = Worksheet1.sorted(a);
+		assertFalse(result);
+	}
+	/*
+	 * Testing sorted(List a) with sorted value list with negatives.
+	 */
+	@Test
+	public void ex2test5(){
+		List a = List.cons(-3, List.cons(-1, List.cons(5,List.empty())));
+		boolean result = Worksheet1.sorted(a);
+		assertTrue(result);
+	}
+	/*
+	 * Testing sorted(List a) with sorted value list with duplicates.
+	 */
+	@Test
+	public void ex2test6(){
+		List a = List.cons(-3, List.cons(-3, List.cons(5,List.empty())));
+		boolean result = Worksheet1.sorted(a);
+		assertTrue(result);
+	}
+	///////////////////////////////////////////////////////////////////////////
+	/*
+	 * Testing merge(List a, List b) with two empty lists.
+	 */
+	@Test
+	public void ex3test1(){
+		List a = List.empty();
+		List b = List.empty();
+		List expected = List.empty();
+		List result = Worksheet1.merge(a,b);
+		assertEquals(expected, result);
+	}
+	/*
+	 * Testing merge(List a, List b) with one empty list.
+	 */
+	@Test
+	public void ex3test2(){
+		List a = List.cons(1,List.cons(2,List.empty()));
+		List b = List.empty();
+		List expected = a;
+		List result = Worksheet1.merge(a,b);
+		assertEquals(expected, result);
+	}
+	/*
+	 * Testing merge(List a, List b) with one empty list.
+	 */
+	@Test
+	public void ex3test3(){
+		List b = List.cons(1,List.cons(2,List.empty()));
+		List a = List.empty();
+		List expected = b;
+		List result = Worksheet1.merge(a,b);
+		assertEquals(expected, result);
+	}
+	/*
+	 * Testing merge(List a, List b).
+	 */
+	@Test
+	public void ex3test4(){
+		
+		List b = List.cons(1, List.cons(3,List.empty()));
+		List a = List.cons(2, List.cons(4,List.empty()));
+		
+		List expected = List.cons(1, List.cons(2, List.cons(3, List.cons(4, List.empty()))));
+		List result = Worksheet1.merge(a,b);
+		System.out.println(Worksheet1.merge(a,b));
+		assertEquals(expected, result);
+	}
+	/*
+	 * Testing merge(List a, List b). UNSORTED LIST
+	 */
+	@Test
+	public void ex3test5(){
+		
+		List b = List.cons(7, List.cons(3,List.empty()));
+		List a = List.cons(2, List.cons(4,List.empty()));
+		try{
+			Worksheet1.merge(a, b);
+			assert(false);
+		} catch (IllegalStateException e) {
+			assert(true);
+		}
+	}
 }
